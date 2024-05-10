b497c8b9fc4e5e34dcea88161caa8c957069abfd
Shafiqullah Turkmen <shafiqullah.turkmen@alo-tech.com>
2024-05-10 23:11:14 +0300

    sidebar structure added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
             <div className="flex flex-1 flex-col gap-6">
                 {
                     sidebarLinks.map((link) => {
-                        const isActive = pathName === link.route || pathName.startsWith(link.route)
+                        const isActive = pathName === link.route || pathName.startsWith(`${link.route}/`)
 
                         return (
                             <Link key={link.label} className={cn(`flex gap-4 items-center p-4 rounded-lg justify-start`, { "bg-blue-1": isActive })} href={link.route}>
