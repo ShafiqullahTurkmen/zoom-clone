c90c6d166a23e067536530334c21f6a5e6c181ee
Shafiqullah Turkmen <shafiqullahturkmen@gmail.com>
2024-05-13 21:41:17 +0300

    clerk auth added

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -16,11 +17,14 @@ const Navbar = () => {
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">Yoom</p>
       </Link>
-        <div className="flex-between gap-5">
-          {/* Clerk User Management */}
+      <div className="flex-between gap-5">
+        {/* Clerk User Management */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
-          <MobileNav />
-        </div>
+        <MobileNav />
+      </div>
     </nav>
   )
 }
